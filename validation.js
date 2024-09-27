c9a9624054ee8b7ad7a3c668ffa3df73c10a08d8
Ifthe16 <habiburrahmanif975@gmail.com>
2024-09-28 03:02:19 +0600

    added validation, date, error and try-catch file of javascript

---
+++ validation.js
@@ -0,0 +1,18 @@
+// const number = '23';
+// string, number, boolean
+const number = 23;
+if (typeof number === 'number') {
+    console.log('value is a number');
+}
+else {
+    console.log('value is not a number');
+}
+
+const numbers = [3, 12, 78, 90];
+console.log(Array.isArray(numbers));
+console.log(typeof numbers);
+
+const student = { name: 'Rongila Rafsan', job: 'khawadawa kora' }
+console.log(typeof student);
+console.log(Array.isArray(student));
+
